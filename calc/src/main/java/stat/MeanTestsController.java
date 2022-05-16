5d705ce6d6fb8805f2928adc5c7dcc576ebed769
Colorblind22 <jaydendierker@gmail.com>
2022-05-16 09:02:19 -0500

    the one sample t works, however, your mother

--- calc/src/main/java/stat/MeanTestsController.java
+++ calc/src/main/java/stat/MeanTestsController.java
@@ -9,6 +9,8 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
 
+import static java.lang.System.out;
+
 public class MeanTestsController 
 {
     @FXML private TextField mu_input;
@@ -16,6 +18,9 @@ public class MeanTestsController
     @FXML private TextField sx_input;
     @FXML private TextField n_input;
 
+
+    @FXML private TextField xbar1_input;
+    @FXML private TextField xbar2_input;
     @FXML private TextField sd1_input;
     @FXML private TextField sd2_input;
     @FXML private TextField n1_input;
@@ -46,16 +51,37 @@ public class MeanTestsController
         double xbar = Double.parseDouble(xbar_input.getText());
         double sx = Double.parseDouble(sx_input.getText());
         double n = Double.parseDouble(n_input.getText());
+        out.printf("inputs:\n\tmu: %.6f\n\txbar: %.6f\n\tsx: %.6f\n\tn: %.6f\n", mu,xbar,sx,n);
 
         TDistribution dist = new TDistribution(n-1);
         double t = (xbar-mu)/(sx/Math.sqrt(n));
-        double eval = dist.probability(t);
+        double eval = dist.cumulativeProbability(t);
+        out.printf("results :\n\tt: %.6f\n", t);
         eval = process() == 2d ? eval*2 : Math.abs(process() +eval); // i forgor 💀
+        out.printf("\tp-value: %.6f\n", eval);
         label.setText(String.format("%.6f", eval));
     }
+    
+    private double two_sample_df(double s1, double n1, double s2, double n2)
+    {
+        double v1 = Math.pow(s1, 2);
+        double v2 = Math.pow(s2, 2);
+        return (Math.pow((v1/n1 + v2/n2),2))/(Math.pow(v1/n1,2)/(n1-1)+Math.pow(v2/n2,2)/n2-1);
+    }
+    
     @FXML public void two_sample_t() throws IOException
     {
-        label.setText(String.format("%.6f", process() == 2d ? new TDistribution(Double.parseDouble(n_input.getText())-1).cumulativeProbability((Double.parseDouble(xbar_input.getText())-Double.parseDouble(mu_input.getText()))/Math.sqrt((Math.pow(Double.parseDouble(sd1_input.getText()),2)/Double.parseDouble(n1_input.getText()))+((Math.pow(Double.parseDouble(sd2_input.getText()),2)/Double.parseDouble(n2_input.getText()))))*2) : Math.abs(process() + 0)));
+        //label.setText(String.format("%.6f", process() == 2d ? new TDistribution(Double.parseDouble(n_input.getText())-1).cumulativeProbability((Double.parseDouble(xbar_input.getText())-Double.parseDouble(mu_input.getText()))/Math.sqrt((Math.pow(Double.parseDouble(sd1_input.getText()),2)/Double.parseDouble(n1_input.getText()))+((Math.pow(Double.parseDouble(sd2_input.getText()),2)/Double.parseDouble(n2_input.getText()))))*2) : Math.abs(process() + 0)));
+        double xbar1=Double.parseDouble(xbar1_input.getText());
+        double sd1=Double.parseDouble(sd1_input.getText());
+        double n1=Double.parseDouble(n1_input.getText());
+        double xbar2=Double.parseDouble(xbar2_input.getText());
+        double sd2=Double.parseDouble(sd2_input.getText());
+        double n2=Double.parseDouble(n2_input.getText());
+        out.printf("inputs:\n\t");
+        double df = two_sample_df(sd1, n1, sd2, n2);
+        
+        TDistribution dist = new TDistribution(df);
     }
 
     @FXML public void back() throws IOException
